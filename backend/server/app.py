2082e5c89995a5aaa8fcf6455ef6b2a639c01547
jaocbhebbel <jacob.hebbel@gmail.com>
2025-11-15 21:35:27 -0500

    added inits for easy access in server

---
+++ backend/server/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from model import classifier, llm
+app = Flask(__name__)
+
+# ping to test connection
+@app.route("/", methods=["GET"])
+def serve():
+    """serve frontend"""
+    return "success", 200
+
+# upload route for chat
+@app.route("/api", methods=["POST"])
+def chat():
+    """sends message to model"""
+
+    try:
+        data = request.get_json()
+        message = data['chat']
+        intent = classifier.getIntent(message)
+        params = llm.parseParameters(intent, message)
+        res = BB.query(intent, params)
+        
+        return jsonify(res), 200
+
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500
+
+    
+if __name__ == "__main__":
+    app.run(port=4000)
