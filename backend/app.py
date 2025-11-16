2523d4d6c75c089c3499ec91d3eeff8d2ab7bccb
jaocbhebbel <jacob.hebbel@gmail.com>
2025-11-16 08:30:41 -0500

    added ngrok

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from model.classifier import IntentClassifier
 from model.llm import ExtractorGPT
+import httpx
+
 app = Flask(__name__)
 
 # ping to test connection
@@ -12,17 +14,33 @@ def serve():
 
 # upload route for chat
 @app.route("/api", methods=["POST"])
-def chat():
+async def chat():
     """sends message to model"""
 
     try:
         data = request.get_json()
         message = data['chat']
-        intent = classifier.getIntent(message)
-        params = llm.parseParameters(intent, message)
-        # res = BB.query(intent, params)
         
-        return jsonify(params), 200
+        intent = classifier.getIntent(message)
+        print(intent)
+        
+        params = llm.parseParameters(intent, message)
+        print(params)
+        
+        async with httpx.AsyncClient() as client:
+
+            endpoint = None
+            if intent == 'get_stat':
+                endpoint = '/lookup'
+            elif intent == 'get_chart':
+                endpoint = '/chart'
+            else:
+                endpoint = '/ping'
+
+            resp = await client.post(f"https://60434d3ecd84.ngrok-free.app{endpoint}", json=params)
+            bloomberg = resp.json()
+
+        return jsonify(bloomberg), 200
 
     except Exception as e:
         print(e)
